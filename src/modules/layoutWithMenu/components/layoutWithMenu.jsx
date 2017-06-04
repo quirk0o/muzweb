e07c8ce8486b43f0a3650b839dfe1ac4c309d883
Beata Obrok <beata.obrok@u2i.com>
2017-06-04 16:58:15 +0200

    Add searching for albums

--- src/modules/layoutWithMenu/components/layoutWithMenu.jsx
+++ src/modules/layoutWithMenu/components/layoutWithMenu.jsx
@@ -5,6 +5,7 @@ import {NavLink} from 'react-router-dom'
 
 import './layoutWithMenu.scss'
 import TrackService from '../../../services/TrackService'
+import AlbumService from '../../../services/AlbumService'
 
 class LayoutWithMenu extends React.Component {
 
@@ -12,7 +13,8 @@ class LayoutWithMenu extends React.Component {
     super()
     this.state = {
       trackPrefix: '',
-      tracks: []
+      tracks: [],
+      albums: []
     }
 
     this.onSearchPrefixChange = this.onSearchPrefixChange.bind(this)
@@ -22,31 +24,43 @@ class LayoutWithMenu extends React.Component {
   onSearchPrefixChange(event, prefix) {
     if (prefix === '') return
     TrackService.searchForTrack(prefix).then(tracks => this.setState({tracks}))
+    AlbumService.searchForAlbum(prefix).then(albums => this.setState({albums}))
   }
 
   onResultSelect(event, data) {
     console.log(data)
-    this.context.router.history.push(`/track/${data.id}`)
+    this.context.router.history.push(`/${data.category}/${data.id}`)
   }
 
   render() {
-    const searchResults = this.state.tracks
+    const trackResults = this.state.tracks
       .map(el => {
         const authorName = el.author.firstName + ' ' + (el.author.lastName || '')
-        return {title: el.name, description: `Author: ${authorName}`, id: el.id}
+        return {title: el.name, description: `Author: ${authorName}`, id: el.id, category: 'tracks'}
       })
+    const albumResults = this.state.albums
+      .map(el => {
+        const authorName = el.author.firstName + ' ' + (el.author.lastName || '')
+        return {title: el.name, description: `Author: ${authorName}`, id: el.id, category: 'albums'}
+      })
+    const searchResults = {albums: {name: 'Albums', results: albumResults}, tracks: {name: 'Tracks', results: trackResults}}
+    console.log(searchResults)
     return (
       <div className="layout container">
         <Container className="app-container">
           <Menu color="teal" className="top navbar">
             <Menu.Item as={NavLink} exact to="/home">Muzweb</Menu.Item>
             <Menu.Item as={NavLink} exact to="/addTrack">Add Track</Menu.Item>
+            <Menu.Item as={NavLink} exact to="/addAlbum">Add </Menu.Item>
             <Menu.Item as={NavLink} exact to="/addAuthor">Add Artist</Menu.Item>
             <Menu.Item as={NavLink} exact to="/addNews">Add News</Menu.Item>
             <div className="menu-right">
-              <Search onSearchChange={this.onSearchPrefixChange}
-                      results={searchResults}
-                      onResultSelect={this.onResultSelect} />
+              <Search
+                category
+                onSearchChange={this.onSearchPrefixChange}
+                results={searchResults}
+                onResultSelect={this.onResultSelect}
+              />
               <Menu.Item as={NavLink} to="/">Logout</Menu.Item>
             </div>
           </Menu>
